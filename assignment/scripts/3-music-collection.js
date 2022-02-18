48c0d006e611847abf2e1349d81d6e6291484a88
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-17 19:56:32 -0600

    updated search function and started tracks stretch goal

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,20 +1,21 @@
 console.log('***** Music Collection *****')
 let collection = [];
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
   let album = {
     title: title,
     artist: artist,
-    yearPublished: yearPublished
+    yearPublished: yearPublished,
+    tracks: tracks
   }
   collection.push(album);
   return album;
 }
-console.log(addToCollection('Feels Like Today', 'Rascal Flatts', '2004'));
-console.log(addToCollection('Dig Your Roots', 'Florida Georgia Line', '2016'));
-console.log(addToCollection('Take Care', 'Drake', '2018'));
-console.log(addToCollection('Me and My Gang', 'Rascal Flatts', '2006'));
-console.log(addToCollection('Live Like You Were Dying', 'Tim McGraw', '2004'));
-console.log(addToCollection('Red', 'Taylor Swift', '2012'));
+console.log(addToCollection('Feels Like Today', 'Rascal Flatts', '2004', ['Here Comes Goodbye', '4:01']));
+console.log(addToCollection('Dig Your Roots', 'Florida Georgia Line', '2016', ['Long Live', '2:32']));
+console.log(addToCollection('Take Care', 'Drake', '2018', ['Time Flies', '2:45']));
+console.log(addToCollection('Me and My Gang', 'Rascal Flatts', '2006', ['God Blessed The Broken Road', '3:03']));
+console.log(addToCollection('Live Like You Were Dying', 'Tim McGraw', '2004', ['My Next Thirty Years', '4:35']));
+console.log(addToCollection('Red', 'Taylor Swift', '2012', ['White Horse', '3:38']));
 console.log(collection);
 
 function showCollection(array) {
@@ -38,22 +39,27 @@ console.log(findByArtist('Florida Georgia Line'));
 console.log(findByArtist('T-Pain'));
 console.log(findByArtist('Rascal Flatts'));
 
-function search(searchArtist, searchYear) {
+const albumSearch = {
+  artist: 'Ray Charles',
+  year: '1957'
+  }
+  const albumMatch = {
+    artist: 'Florida Georgia Line',
+    year: '2016'
+    }
+function search(searchCriteria) {
 let searchArray = [];
-const searchCriteria = {
-  artist: searchArtist,
-  year: searchYear
-}
-if (searchCriteria.artist === undefined && searchCriteria.year === undefined) {
-  return collection;
+
+if (searchCriteria === undefined) {
+return collection;
 }
 for (let i = 0; i < collection.length; i++) {
-  if (collection[i].artist === searchArtist && collection[i].yearPublished === searchYear) {
+  if (collection[i].artist === searchCriteria.artist && collection[i].yearPublished === searchCriteria.year) {
     searchArray.push(collection[i]);
   }
 }
 return searchArray;
 }
-console.log(search('Ray Charles', '1957'));
+console.log(search(albumSearch));
 console.log(search());
-console.log(search('Florida Georgia Line', '2016'));
+console.log(search(albumMatch));
